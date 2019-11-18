d393bafd2e6a7f092143c086bd804c0116ab1f8c
Maks <nazirov.maksat96@gmail.com>
2019-11-18 12:38:28 +0600

    part5

---
+++ P5_T1.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the funnyString function below.
+def isFunny(word):
+    i = 0
+    length = len(word)
+    arr = []
+    revarr = []
+    for i in range(0,math.ceil(length/2)):
+        sdiff = abs(ord(word[i])-ord(word[i+1]))
+        rdiff = abs(ord(word[length-i-1])-ord(word[length-i-2]))
+        if sdiff == rdiff:
+               continue
+        else:
+               return False
+    return True
+
+if __name__ == "__main__":
+    n = int(input())
+    for _ in range(n):
+        word = input()
+        if isFunny(word):
+            print("Funny")
+        else:
+            print("Not Funny")
+
+            
